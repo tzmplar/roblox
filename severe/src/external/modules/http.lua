fdee94d3b9f4a85cbd5a77e1b052b6f6412c5a9d
tzmplar <veilcure@outlook.com>
2025-08-22 02:05:26 +0300

    money

--- severe/src/external/modules/http.lua
+++ severe/src/external/modules/http.lua
@@ -3,17 +3,25 @@ return {
         assert("table" == type(settings), `http.get: settings must be a table, got {type(settings)}`)
         assert("function" == type(callback), `http.get: callback must be a function, got {type(callback)}`)
 
-        spawn(function()
-            return callback(httpget(unpack(settings)))
-        end)
+        if callback then
+            return spawn(function()
+                return callback(httpget(unpack(settings)))
+            end)
+        else
+            return httpget(unpack(settings))
+        end
     end,
 
     post = function(settings, callback)
         assert("table" == type(settings), `http.get: settings must be a table, got {type(settings)}`)
         assert("function" == type(callback), `http.get: callback must be a function, got {type(callback)}`)
 
-        spawn(function()
-            return callback(httpget(unpack(settings)))
-        end)
+        if callback then
+            return spawn(function()
+                return callback(httppost(unpack(settings)))
+            end)
+        else
+            return httppost(unpack(settings))
+        end
     end
 }
